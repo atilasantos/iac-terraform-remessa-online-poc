d757d2aa60da1645cd8bc081d1423b1ff2816335
Atila Santos <atila.santos@telefonica.com>
2020-10-25 17:17:09 -0300

    Removing sensitive info

--- remote-state.tf
+++ remote-state.tf
@@ -1,10 +1,10 @@
 terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "atilaPocs"
+  backend "local" {}#{
+  #   hostname = "app.terraform.io"
+  #   organization = "atilaPocs"
 
-    workspaces {
-      name = "iac-terraform-remessa-online-poc"
-    }
-  }
+  #   workspaces {
+  #     name = "iac-terraform-remessa-online-poc"
+  #   }
+  # }
 }
