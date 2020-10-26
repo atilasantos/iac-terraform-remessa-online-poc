f9668f286d31108fe69cc88f75c637e3e647852c
Atila Santos <atila.santos@telefonica.com>
2020-10-26 16:32:00 -0300

    OFF-TOPIC - Adding a script validation to check elb_dns_name availability

---
+++ validate_availability.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+command=`curl $1`
+while [ -z "${command}" ]; do
+    clear
+    echo -e "\e[1;31m ELB not resolved.. \e[0m"
+    command=`curl $1`
+    sleep 2
+
+done
+
+echo -e "\e[1;32m ELB finally resolved! \e[0m"
+
+google-chrome $1
